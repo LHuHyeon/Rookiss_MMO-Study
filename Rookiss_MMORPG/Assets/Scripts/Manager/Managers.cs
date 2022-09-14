378887efc5282218db51553845f67a7419dcbd1d
LHuHyeon <minfkffn@gmail.com>
2022-09-14 21:23:34 +0900

    프로젝트 생성
    1. @Manager 싱글톤 추가

---
+++ Rookiss_MMORPG/Assets/Scripts/Manager/Managers.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Managers : MonoBehaviour
+{
+    static Managers s_instance; // 유일성이 보장된다.
+    public static Managers Instance { get { Init(); return s_instance; } }  // 유일한 매니저를 갖고 온다.
+    
+    void Start()
+    {
+        Init();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    // 싱글톤 메소드
+    static void Init()
+    {
+        if (s_instance == null){
+            GameObject go = GameObject.Find("@Manager");// 오브젝트 찾기
+
+            if (go == null){
+                go = new GameObject{name = "@Manager"}; // 오브젝트 이름 설정
+                go.AddComponent<Managers>();            // 컴포넌트 추가
+                Debug.Log("@Manager 생성.");
+            }
+
+            DontDestroyOnLoad(go);                      // 씬변경될 때 삭제 안됨.
+            s_instance = go.GetComponent<Managers>();
+        }
+    }
+}
